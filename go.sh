4e5eda52b3cbe3c0e176f370c569e24f83b44b24
mzandrew <mza@phys.hawaii.edu>
2019-05-03 08:15:46 -1000

    moving to correct number of photons per step; other minor changes

--- go.sh
+++ go.sh
@@ -5,12 +5,13 @@ cd $localdir
 mkdir -p build
 cd build
 cmake ..
-make
+make -j2
 . /usr/local/bin/geant4.sh
 . ${HOME}/build/root/bin/thisroot.sh
 
 for HL in H L; do
 	for situation in bulk_si edge_on face_on; do
+		echo "./${situation}_${HL}ER ${HL}ER-N-bunches.mac > ${HL}ER-${situation}.log"
 		./${situation}_${HL}ER ${HL}ER-N-bunches.mac > ${HL}ER-${situation}.log
 		grep -v " 0.0$" ${HL}ER-${situation}.log | grep "^[0-9]" | sort -n > ${HL}ER-${situation}
 		#grep -cvH " 0.0$" ${HL}ER-${situation}.log || /bin/true
