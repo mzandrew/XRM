124e6d7ebd2d4c3080cb9f925de4d3dd0081ad19
mzandrew <mza@phys.hawaii.edu>
2019-05-10 14:27:52 -1000

    plotting all the major absorbers now; associated changes

--- spectra.py
+++ spectra.py
@@ -21,7 +21,9 @@ import re # search
 import math # log10
 import numpy # float array
 
-number_of_bins = 1000
+skim = 10
+stop_short = False
+number_of_bins = 200
 low = 0.004
 high = 50.
 factor = 10.**(math.log10(high/low)/number_of_bins)
@@ -35,24 +37,45 @@ bin_widths.append(high)
 #print str(number_of_bins)
 #print bin_widths
 fbin_widths = numpy.array(bin_widths, dtype='float64')
+epsilon_eV = 1.
+legend1 = ROOT.TLegend(0.1, 0.6, 0.4, 0.9)
 
 J_per_eV = 1.60217733e-19
 J_per_MeV = 1.0e6 * J_per_eV
 bunches_per_second = 508.8875e6
 
-def show_energy_per_bunch_and_power():
-	print "total_energy_incident " + str(total_energy_incident_eV/1.e6) + " MeV per bunch"
-	print "total_energy_deposited " + str(total_energy_deposited_eV/1.e6) + " MeV per bunch"
+total_energy_deposited_J = {}
+total_power_deposited_W = {}
+def show_energy_per_bunch_and_power(string):
+	#print "total_energy_incident " + str(total_energy_incident_eV/1.e6) + " MeV per bunch"
 	total_energy_incident_J = J_per_MeV * total_energy_incident_eV / 1.e6
-	total_energy_deposited_J = J_per_MeV * total_energy_deposited_eV / 1.e6
-	print "total_energy_incident " + str(total_energy_incident_J) + " J per bunch"
-	print "total_energy_deposited " + str(total_energy_deposited_J) + " J per bunch"
+	#print "total_energy_incident " + str(total_energy_incident_J) + " J per bunch"
 	total_power_incident_W = total_energy_incident_J * bunches_per_second
-	total_power_deposited_W = total_energy_deposited_J * bunches_per_second
-	print "total_power_incident " + str(total_power_incident_W) + " W"
-	print "total_power_deposited " + str(total_power_deposited_W) + " W"
+	print "total_power_incident %.3f W" % total_power_incident_W
+	for key in sorted(total_energy_deposited_eV, key=total_energy_deposited_eV.get, reverse=True):
+		match = re.search(string, key)
+		if match:
+			#print "total_energy_deposited[" + key + "] " + str(total_energy_deposited_eV[key]/1.e6) + " MeV per bunch"
+			total_energy_deposited_J[key] = J_per_MeV * total_energy_deposited_eV[key] / 1.e6
+			#print "total_energy_deposited[" + key + "] " + str(total_energy_deposited_J[key]) + " J per bunch"
+			total_power_deposited_W[key] = total_energy_deposited_J[key] * bunches_per_second
+			print "total_power_deposited[" + key + "] %.3f W" % total_power_deposited_W[key]
+		else:
+			print key
 
-legend1 = ROOT.TLegend(0.1, 0.75, 0.45, 0.9)
+def parse_string(string):
+#	print string
+	match = re.search(" +([.e0-9-]+) ([:a-zA-Z0-9]+)(.*)$", string)
+	#deposited_energy_eV = 0.0
+	#tag = "BLANK"
+	#not_done = len(remaining_string)
+	if match:
+		deposited_energy_eV = float(match.group(1))
+		tag = match.group(2)
+		remaining_string = match.group(3)
+		not_done = len(remaining_string)
+	return (deposited_energy_eV, tag, not_done, remaining_string)
+
 #if __name__ == "__main__":
 filenames = []
 import sys
@@ -63,12 +86,13 @@ else:
 	png_filename = sys.argv[1]
 	for each in sys.argv[2:]:
 		filenames.append(each)
-histograms = []
+histograms = {}
 i = 0
 title = " vs ".join(filenames)
 histogram_stack = ROOT.THStack("histogram_stack", title)
+
 for filename in filenames:
-	lines = 0
+	print "reading file " + filename + "..."
 	#print filename
 	#lines = []
 	#for line in open(filename):
@@ -77,50 +101,136 @@ for filename in filenames:
 	#input_energies_eV = {}
 	#deposited_energies_eV = {}
 	total_energy_incident_eV = 0.0
-	total_energy_deposited_eV = 0.0
-	histograms.append(ROOT.TH1F('histogram['+str(i)+']', title, number_of_bins, fbin_widths))
+	total_energy_deposited_eV = {}
+	histogram_initiated = 0
+	#total_lines = sum(1 for line in open(filename))
+	lines = 0
+	matching_lines = 0
 	for line in open(filename):
-		line = line.rstrip("\n\r")
-		match = re.search("^([0-9]+) +([.e0-9-]+) .* +([.e0-9-]+) Si(EdgeOn|FaceOn|Bulk).*", line)
-		if match:
-			#print line
-			#event_number = int(match.group(1))
-			input_energy_eV = float(match.group(2))
-			deposited_energy_eV = float(match.group(3))
-			total_energy_incident_eV += input_energy_eV
-			total_energy_deposited_eV += deposited_energy_eV
-			#print str(event_number) + " " + str(input_energy) + " " + str(deposited_energy)
-			#input_energies_eV[event_number] = input_energy_eV
-			#deposited_energies_eV[event_number] = deposited_energy_eV
-			histograms[i].Fill(deposited_energy_eV/1000.0)
 		lines = lines + 1
-	print "read " + str(lines) + " lines from file " + filename
-	show_energy_per_bunch_and_power()
+		if not 0==lines%skim:
+			continue
+		line = line.rstrip("\n\r")
+		# evt# incident_E E1 process1 E2 process2 ... depE_a object_a depE_b object_b ...
+		# 1 36.8176 13.63 phot 23.1876 eIoni 0 Air 0 BeFilter 36.8176 SiBulk
+		match = re.search("^([0-9]+) +([.e0-9-]+)(.*)$", line)
+		if match:
+			matching_lines = matching_lines + 1
+			event_number = int(match.group(1))
+			incident_energy_eV = float(match.group(2))
+			total_energy_incident_eV += incident_energy_eV
+			remaining_string = match.group(3)
+			not_done = len(remaining_string)
+			while not_done:
+				(deposited_energy_eV, tag, not_done, remaining_string) = parse_string(remaining_string)
+				match = re.search("(phot|eIoni|msc|compt|eBrem|Rayl)", tag)
+				if match:
+					continue
+				match = re.search("(HeBox)", tag)
+				if match:
+					continue
+				if deposited_energy_eV < epsilon_eV:
+					continue
+				name = filename + "_" + tag
+				match = re.search("bulk_si_Air", name)
+				if match:
+					continue
+				match = re.search("bulk_si_BeFilter", name)
+				if match:
+					continue
+				match = re.search("ER-face_on_(BeWindow|Air|BeFilter|CopperSlit)", name)
+				if match:
+					continue
+				try:
+					total_energy_deposited_eV[name] += deposited_energy_eV
+				except:
+					total_energy_deposited_eV[name] = deposited_energy_eV
+				try:
+					histograms[name].Fill(deposited_energy_eV/1000.0)
+				except:
+					histograms[name] = ROOT.TH1F(name, title, number_of_bins, fbin_widths)
+					histograms[name].Fill(deposited_energy_eV/1000.0)
+			#print str(event_number) + " " + str(incident_energy) + " " + str(deposited_energy)
+			#incident_energies_eV[event_number] = incident_energy_eV
+			#deposited_energies_eV[event_number] = deposited_energy_eV
+			if 0==matching_lines%100000:
+				print "read " + str(matching_lines) + " lines from file " + filename + " so far..."
+			if stop_short:
+				if 300000==matching_lines:
+					break
+	print "read " + str(matching_lines) + " lines from file " + filename + " total"
+	show_energy_per_bunch_and_power(filename)
+
 	#for event in input_energies.keys():
 	#	histograms[i].Fill(input_energies[event])
 	#for event in deposited_energies_eV.keys():
-	legend1.AddEntry(histograms[i], filename + " (" + str(int(histograms[i].GetEntries())) + " entries)")
 	#normalization = histograms[i].GetEntries()
 	#histograms[i].Scale(1./normalization)
 	#histograms[i].GetYaxis().SetTitle("relative abundance")
 	#histograms[i].SetLineColor(ROOT.kWhite + i)
 	#histograms[i].SetLineColor(ROOT.kGray + i)
 	#histograms[i].SetFillColor(ROOT.kGray + i)
-	if 0==i:
-		histograms[i].SetLineColor(ROOT.kGray)
-		histograms[i].SetFillColor(ROOT.kGray)
-#		histograms[0].Draw()
-	elif 1==i:
-		histograms[i].SetLineColor(ROOT.kBlue)
-	else:
-		histograms[i].SetLineColor(ROOT.kRed)
-#		histograms[i].Draw("same")
-	histogram_stack.Add(histograms[i])
-	i = i + 1
-canvas1 = ROOT.TCanvas('canvas1', 'mycanvas', 100, 50, 800, 600)
+
+#	j = 0
+#	for key in histograms.keys():
+#		if 0==i:
+#			histograms[key].SetLineColor(ROOT.kGreen + j)
+#		elif 1==i:
+#			histograms[key].SetLineColor(ROOT.kBlue + j)
+#		elif 2==i:
+#			histograms[key].SetLineColor(ROOT.kRed + j)
+#		match = re.search("SiBulk", key)
+#		if match:
+#		j = j + 1
+##	else:
+##		histograms[i].SetLineColor(ROOT.kRed)
+##		histograms[i].Draw("same")
+#	i = i + 1
+#	for key in histograms.keys():
+#		histogram_stack.Add(histograms[key])
+
+for key in histograms.keys():
+	match = re.search("SiBulk", key)
+	if match:
+		histograms[key].SetLineColor(ROOT.kGray)
+		histograms[key].SetFillColor(ROOT.kGray)
+		histogram_stack.Add(histograms[key])
+		legend1.AddEntry(histograms[key], "%.3f W %s (%d entries)" % (total_power_deposited_W[key], key, histograms[key].GetEntries()))
+j = 0
+for key in histograms.keys():
+	match = re.search("(SiBulk|SiEdgeOn|SiFaceOn)", key)
+	if not match:
+		histograms[key].SetLineColor(ROOT.kGreen + j)
+		match = re.search("Be(Window|Filter)", key)
+		if match:
+			histograms[key].SetLineColor(ROOT.kMagenta)
+		match = re.search("Copper", key)
+		if match:
+			histograms[key].SetLineColor(ROOT.kOrange)
+		match = re.search("Air", key)
+		if match:
+			histograms[key].SetLineColor(ROOT.kBlack)
+		match = re.search("SiBeamDump", key)
+		if match:
+			histograms[key].SetLineColor(ROOT.kGray+3)
+		histogram_stack.Add(histograms[key])
+		legend1.AddEntry(histograms[key], "%.3f W %s (%d entries)" % (total_power_deposited_W[key], key, histograms[key].GetEntries()))
+		j = j + 1
+for key in histograms.keys():
+	match = re.search("(SiEdgeOn|SiFaceOn)", key)
+	if match:
+		match = re.search("SiEdgeOn", key)
+		if match:
+			histograms[key].SetLineColor(ROOT.kBlue)
+		match = re.search("SiFaceOn", key)
+		if match:
+			histograms[key].SetLineColor(ROOT.kRed)
+		histogram_stack.Add(histograms[key])
+		legend1.AddEntry(histograms[key], "%.3f W %s (%d entries)" % (total_power_deposited_W[key], key, histograms[key].GetEntries()))
+canvas1 = ROOT.TCanvas('canvas1', 'mycanvas', 100, 50, 1280, 1024)
 #canvas1.SetBatch(1)
 canvas1.SetLogx()
-#canvas1.SetLogy()
+canvas1.SetLogy()
 histogram_stack.Draw("nostack")
 histogram_stack.GetXaxis().SetTitle("deposited energy [keV]")
 histogram_stack.GetYaxis().SetTitle("number of events")
