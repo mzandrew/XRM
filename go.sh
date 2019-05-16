6daae8ce0f59367c0ae454e247e6608eae9ee367
mzandrew <mza@phys.hawaii.edu>
2019-05-16 11:07:53 -1000

    spreading build and generate steps across as many cpus as it finds in /proc/cpuinfo

--- go.sh
+++ go.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 declare -i build=1 generate=1 plot=1
-declare -i generate_quick=1
+declare -i parallel_generate=1
+declare -i num_cpus=$(grep -c "^processor" /proc/cpuinfo)
 
 declare localdir=$(cd $(dirname $(readlink -f $0)); pwd)
 cd $localdir
@@ -10,7 +11,7 @@ cd build
 if [ $build -gt 0 ]; then
 	. /usr/local/share/Geant4-10.5.1/geant4make/geant4make.sh
 	cmake ..
-	make -j2
+	make -j$num_cpus
 fi
 
 #echo "$ROOTSYS $PYTHONPATH"
@@ -22,14 +23,28 @@ fi
 #declare situation_list="bulk_si edge_on edge_on_CeYAG face_on"
 #declare situation_list="bulk_si edge_on edge_on_scint edge_on_scint_gold"
 declare situation_list="edge_on edge_on_scint edge_on_scint_gold"
+num_cpus=$((num_cpus/2)) # half HER, half LER
 
 function do_ring {
-	local HL=${1}
+	local HL="${1}"
+	local -i N=0
+	local bpid_list=""
 	if [ $generate -gt 0 ]; then
 		for situation in $situation_list; do
 			echo "./${situation}_${HL}ER ${HL}ER-N-bunches.mac > ${HL}ER-${situation}"
-			./${situation}_${HL}ER ${HL}ER-N-bunches.mac > ${HL}ER-${situation}
+			if [ $parallel_generate -gt 0 ] || [ $N -ge $num_cpus ]; then
+				./${situation}_${HL}ER ${HL}ER-N-bunches.mac > ${HL}ER-${situation} &
+				bpid_list="$bpid_list $!"
+				N=$((N+1))
+			else
+				./${situation}_${HL}ER ${HL}ER-N-bunches.mac > ${HL}ER-${situation}
+			fi
 		done
+		if [ $parallel_generate -gt 0 ]; then
+			for bpid in $bpid_list; do
+				wait $bpid
+			done
+		fi
 	fi
 	if [ $plot -gt 0 ]; then
 		filename="XRM.${HL}ER.png"
@@ -40,7 +55,7 @@ function do_ring {
 	fi
 }
 
-if [ $generate_quick -gt 0 ]; then
+if [ $parallel_generate -gt 0 ]; then
 	echo "running HER simulation in background..."
 	do_ring H >> output-HER.log &
 	echo "running LER simulation..."
