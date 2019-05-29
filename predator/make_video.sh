325fc15365b2d59aab440952415cbab1163d3336
mzandrew <mza@phys.hawaii.edu>
2019-05-29 12:21:26 -1000

    adding gnuplot script to plot temperature vs time; adding ffmpeg script to composite time series of images and add overlays with synchronized cursor

---
+++ predator/make_video.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+#declare output_size="1920x1080"
+declare output_size="3840x2160"
+declare output_filename="XRM-HER-first-day.mp4"
+declare -i xi=275 w=425
+declare -i yi=77 h=170
+declare -i last_frame_number=701
+declare -i fps=60
+
+#time ffmpeg -r 60 -f image2 -pattern_type glob -i "*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p -s $output_size $output_filename
+#time ffmpeg -r 60 \
+#	-f image2 -pattern_type glob -i "*.jpg" \
+#	-i ~/build/2019-05-29.1430.superkekb.dailysnap.gif -filter_complex "[0:v][1:v] overlay=0:0" \
+#	-vcodec libx264 -crf 25  -pix_fmt yuv420p -s $output_size $output_filename
+
+declare -i line_height=$((750+260))
+declare slope=$(echo "$w/$last_frame_number" | bc -l)
+declare -i step=$(echo "(1/$slope)/1" | bc)
+if [ $step -eq 0 ]; then
+	step=1
+fi
+#echo "slope=$slope step=$step"
+drawline_string=""
+for n in $(seq 0 $step $((last_frame_number-1))); do
+	if [ $n -gt 0 ]; then
+		drawline_string="$drawline_string, "
+	fi
+	drawline_string="$drawline_string drawbox=enable='between(n,$n,$((n+step)))':x=$(echo "($xi+$slope*$n+0.5)/1" | bc):y=0:w=3:h=$line_height:c=blue"
+done
+#echo "$drawline_string"
+#exit 0
+
+time ffmpeg \
+	-r $fps \
+	-f image2 \
+	-pattern_type glob -i "*.jpg" \
+	-i ~/build/XRM/predator/pictures/2019-05-29.1430.superkekb.dailysnap.gif \
+	-i ~/build/XRM/predator/pictures/temperatures.png \
+	-filter_complex "[0:v][1:v] overlay=0:0[a], [a][2:v] overlay=0:750, $drawline_string, drawbox=x=$xi:y=$yi:w=$w:h=$h:c=green:t=2" \
+	-vcodec libx264 \
+	-crf 25 \
+	-pix_fmt yuv420p \
+	-s $output_size \
+	$output_filename
+echo; echo "mplayer -loop 0 $output_filename"
+	#-filter_complex "[0:v][1:v] overlay=0:0, drawbox=$xi:$yi:$w:$h:green, drawbox=$xi+$slope*t/$fps:0:3:750:blue" \
+	#, [c]trim=0:1[hold];[c][hold]concat[extended];[extended][c]overlay
+	#[c], [c]trim=0:1[hold];[c][hold]concat[extended];[extended][c]overlay
+ffmpeg -ss 00:00:11.50 -i $output_filename -frames:v 1 $output_filename.jpg
+
---
+++ predator/temperatures.gnuplot
@@ -0,0 +1,20 @@
+set xdata time
+set timefmt "%Y-%m-%d.%H%M%S"
+#set format x "%Y\n%m/%d\n%H:%M"
+#set format x "%H:%M"
+set format x "%Hh"
+set key right center
+set xrange ['2019-05-28.143000' : '2019-05-29.143000']
+
+set terminal png size 1022,260
+set output "pictures/temperature.png"
+set lmargin 9
+set rmargin 12
+#set style line 1 linewidth 40
+
+#plot "temperatures.log" using 1:2 title "ambient", "temperatures.log" using 1:(22+$2-$3) with linespoints title "copper"
+plot "temperatures.log" using 1:2 with lines linewidth 2 title "ambient", "temperatures.log" using 1:(47-$3) with lines linewidth 2 title "copper"
+#plot "temperatures.log" using 1:2 with lines linewidth 2 title "ambient", "temperatures.log" using 1:3 with lines linewidth 2 title "copper"
+
+#pause -1
+
