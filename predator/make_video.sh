7e2ae1ac5f81f5e687c98f9cd100a1b3cb048c8e
mzandrew <mza@phys.hawaii.edu>
2019-06-18 07:31:42 -1000

    minor changes for quick video

--- predator/make_video.sh
+++ predator/make_video.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
-#declare output_size="1920x1080"
 declare output_size="3840x2160"
-declare output_filename="XRM-HER-first-day.mp4"
+#output_size="1920x1080"
+output_size="1024x576"
+#output_size="800x450"
+declare output_filename="XRM-HER-next-try.mp4"
 declare -i xi=275 w=425
 declare -i yi=77 h=170
 declare -i last_frame_number=701
-declare -i fps=60
+declare -i fps=1
 
 #time ffmpeg -r 60 -f image2 -pattern_type glob -i "*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p -s $output_size $output_filename
 #time ffmpeg -r 60 \
@@ -30,14 +32,14 @@ for n in $(seq 0 $step $((last_frame_number-1))); do
 done
 #echo "$drawline_string"
 #exit 0
+	#-i ~/build/XRM/predator/pictures/2019-05-29.1430.superkekb.dailysnap.gif \
+	#-i ~/build/XRM/predator/pictures/temperatures.png \
+	#-filter_complex "[0:v][1:v] overlay=0:0[a], [a][2:v] overlay=0:750, $drawline_string, drawbox=x=$xi:y=$yi:w=$w:h=$h:c=green:t=2" \
 
 time ffmpeg \
 	-r $fps \
 	-f image2 \
-	-pattern_type glob -i "*.jpg" \
-	-i ~/build/XRM/predator/pictures/2019-05-29.1430.superkekb.dailysnap.gif \
-	-i ~/build/XRM/predator/pictures/temperatures.png \
-	-filter_complex "[0:v][1:v] overlay=0:0[a], [a][2:v] overlay=0:750, $drawline_string, drawbox=x=$xi:y=$yi:w=$w:h=$h:c=green:t=2" \
+	-pattern_type glob -i "/media/mza/rootfs/home/pi/build/XRM/predator/pictures/2019-06*.jpg" \
 	-vcodec libx264 \
 	-crf 25 \
 	-pix_fmt yuv420p \
@@ -47,5 +49,5 @@ echo; echo "mplayer -loop 0 $output_filename"
 	#-filter_complex "[0:v][1:v] overlay=0:0, drawbox=$xi:$yi:$w:$h:green, drawbox=$xi+$slope*t/$fps:0:3:750:blue" \
 	#, [c]trim=0:1[hold];[c][hold]concat[extended];[extended][c]overlay
 	#[c], [c]trim=0:1[hold];[c][hold]concat[extended];[extended][c]overlay
-ffmpeg -ss 00:00:11.50 -i $output_filename -frames:v 1 $output_filename.jpg
+#ffmpeg -ss 00:00:11.50 -i $output_filename -frames:v 1 $output_filename.jpg
 
