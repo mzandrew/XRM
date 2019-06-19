e971159c9ca267b2795d6e62e0557b4d921ad33c
mza <mza@phys.hawaii.edu>
2019-06-19 09:12:15 +0900

    accumulates 256 samples and averages before reporting

--- predator/samd21_get_temperature.py
+++ predator/samd21_get_temperature.py
@@ -8,6 +8,7 @@ led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 led.value = True
 
+import sys
 import time
 import array
 import math
@@ -18,7 +19,7 @@ analog0 = AnalogIn(board.A0)
 analog1 = AnalogIn(board.A1)
 #import neopixel
 
-NUM_SAMPLES = 160
+#NUM_SAMPLES = 160
 
 #pixel = neopixel.NeoPixel(board.A1, 8, 0.3, True)
 #pixel.fill(0, 0, 255)
@@ -33,30 +34,56 @@ def getCurrent(pin):  # helper
     #return ((((pin.value * 3.3) / 65536)/R)*10-175)
     return (getVoltage(pin)/gain/R)
 
-def mean(values):
-    return sum(values) / len(values)
+#def mean(values):
+#    return sum(values) / len(values)
 
-def normalized_rms(values):
-    minbuf = int(mean(values))
-    samples_sum = sum(
-        float(sample - minbuf) * (sample - minbuf)
-        for sample in values
-    )
-    return math.sqrt(samples_sum / len(values))
+#def normalized_rms(values):
+#    minbuf = int(mean(values))
+#    samples_sum = sum(
+#        float(sample - minbuf) * (sample - minbuf)
+#        for sample in values
+#    )
+#    return math.sqrt(samples_sum / len(values))
 
-samples = array.array('H', [0] * NUM_SAMPLES)
-input_floor = normalized_rms(samples) + 10
+#samples = array.array('H', [0] * NUM_SAMPLES)
+#input_floor = normalized_rms(samples) + 10
+
+reporting_frequency = 1.0 # Hz
+log2_of_number_of_acquisitions_to_accumulate = 8
+number_of_acquisitions_to_accumulate = 2.0 ** log2_of_number_of_acquisitions_to_accumulate
+#number_of_acquisitions_to_accumulate = 256
+#print(str(number_of_acquisitions_to_accumulate))
+if number_of_acquisitions_to_accumulate < 1:
+    sys.exit(1)
+short_timestep = 1.0 / number_of_acquisitions_to_accumulate / reporting_frequency # s
+#short_timestep = 0.016
+#print(str(short_timestep))
+#long_timestep  = short_timestep * number_of_acquisitions_to_accumulate / reporting_frequency# s
 
 while True:
     led.value = not led.value
     #pixel.fill(0, 255, 0)
     #time.sleep(0.25)
-    temp_c = thermistor.temperature
     #temp_f = thermistor.temperature * 9.0 / 5.0 + 32.0
     #votlageN = getVoltage(analogin)
     #print("T: %f C and %f F A : %f mV" % (temp_c, temp_f, getVoltage(analogin)))
     #print("%.1f %.3f" % (temp_c, getCurrent(analogin)))
-    print("%.1f %.3f %.3f" % (temp_c, getVoltage(analog0), getVoltage(analog1)))
+    temp_c = 0.0
+    v0 = 0.0
+    v1 = 0.0
+    for i in range(0, number_of_acquisitions_to_accumulate):
+        temp_c += thermistor.temperature
+        v0 += getVoltage(analog0)
+        v1 += getVoltage(analog1)
+        time.sleep(short_timestep)
+    #temp_c >>= log2_of_number_of_acquisitions_to_accumulate
+    #v0 >>= log2_of_number_of_acquisitions_to_accumulate
+    #v1 >>= log2_of_number_of_acquisitions_to_accumulate
+    temp_c /= number_of_acquisitions_to_accumulate
+    v0 /= number_of_acquisitions_to_accumulate
+    v1 /= number_of_acquisitions_to_accumulate
+    diff = 1000.0 * (v1 - v0)
+    print("%.1f %.3f %.3f %.3f" % (temp_c, v0, v1, diff))
     #pixel.fill(255, 0, 0)
-    time.sleep(0.25)
+    #time.sleep(long_timestep)
 
